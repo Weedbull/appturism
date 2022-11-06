6cefe353b94b5358f534e7850a8e209f678691ac
Weedbull <93524480+Weedbull@users.noreply.github.com>
2022-11-06 16:39:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/smartkartapp/About.java
@@ -0,0 +1,12 @@
+package com.example.smartkartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.net.URI;
+
